07b38f127d778986fd3533fab202bb43ba0c4035
ChadDiaz <chadcdiaz@gmail.com>
2021-03-25 14:18:41 -0400

    mvp has been reached

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -19,22 +19,39 @@ router.get("/:id", mw.checkAccountId, async (req, res, next) => {
   }
 });
 
-router.post("/", mw.checkAccountPayload,  async (req, res, next) => {
+router.post("/", mw.checkAccountPayload, async (req, res, next) => {
   const newAcct = req.body;
   try {
     const data = await Accounts.create(newAcct);
-    res.status(200).json(data);
+    res.status(201).json(data);
   } catch (err) {
     next({ error: err, message: err.message, status: 500 });
   }
 });
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
-});
+router.put(
+  "/:id",
+  mw.checkAccountPayload,
+  mw.checkAccountId,
+  async (req, res, next) => {
+    const changes = req.body;
+    const { id } = req.params;
+    try {
+      const update = await Accounts.updateById(id, changes);
+      res.status(202).json(update);
+    } catch (err) {
+      next({ error: err, message: err.message, status: 500 });
+    }
+  }
+);
 
-router.delete("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete("/:id", mw.checkAccountId, async (req, res, next) => {
+  try {
+    await Accounts.deleteById(req.params.id);
+    res.status(204).send("");
+  } catch (err) {
+    next({ error: err, message: err.message, status: 500 });
+  }
 });
 
 router.use((error, req, res, next) => {
